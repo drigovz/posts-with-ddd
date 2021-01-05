e382673f2189e736baf866905060587bfba0bcd5
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-01-05 15:16:30 -0300

    Add CategoryService in Api.Service

---
+++ Api.Service/Services/CategoryService.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Api.Domain.Interfaces.Services.Categories;
+
+namespace Api.Service.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IRepository<Category> _repository;
+
+        public CategoryService(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+        
+        public async Task<IEnumerable<Category>> GetAsync()
+        {
+            return await _repository.GetAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            return await _repository.AddAsync(category);
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            return await _repository.UpdateAsync(category);
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
